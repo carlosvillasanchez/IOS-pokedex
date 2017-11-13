af09f359a5829f46998fcf027800003f27c63b9c
carlosvillasanchez <carlos.villa.vs@gmail.com>
2017-11-13 15:47:21 +0100

    Initial Commit

---
+++ pokedex_fc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex_fc
+//
+//  Created by Carlos Villa on 13/11/2017.
+//  Copyright © 2017 Carlos Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
